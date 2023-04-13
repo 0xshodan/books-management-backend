822c0ea9d4d3d2e15ced5c92cd61fa5cc963e996
0xshodan <hngdean@gmail.com>
2023-04-13 03:12:54 +0300

    add book model

--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Название")
+    title = models.CharField(max_length=30, verbose_name="Заголовок", blank=True, null=True)
+    author = models.CharField(max_length=30, verbose_name="Автор")
+    description = models.CharField(max_length=512, verbose_name="Описание", blank=True, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=0, verbose_name="Цена")
