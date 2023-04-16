9d79d687bcd391ce5ded1e01a446457e37c8132b
0xshodan <hngdean@gmail.com>
2023-04-17 00:49:09 +0300

    disable authorization

--- src/books/api/resources.py
+++ src/books/api/resources.py
@@ -1,8 +1,10 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from books.models import Book
 
 
 class BookResource(ModelResource):
     class Meta:
         queryset = Book.objects.all()
-        resource_name = 'book'
+        resource_name = "book"
+        authorization = Authorization()
